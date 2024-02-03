0fbef1545184dd513ad769458e648e6e1bea0a53
Bhar32arya <bhar32arya@gmail.com>
2024-02-03 17:39:48 +0530

    updated code

--- junit-mockito/src/main/java/com/bhar32/practise/controller/ProductController.java
+++ junit-mockito/src/main/java/com/bhar32/practise/controller/ProductController.java
@@ -6,13 +6,11 @@ import com.bhar32.practise.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
-@RequestMapping("api")
+@RequestMapping("/api")
 public class ProductController {
 
     @Autowired
@@ -22,5 +20,10 @@ public class ProductController {
     public ResponseEntity<ApiResponse> addProduct(@RequestBody Product product) {
         productService.addProduct(product);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/getAllProducts")
+    public ResponseEntity<List<Product>> getProducts() {
+        return productService.getAllProdcts();
     }
 }
--- junit-mockito/src/main/java/com/bhar32/practise/service/ProductService.java
+++ junit-mockito/src/main/java/com/bhar32/practise/service/ProductService.java
@@ -3,7 +3,12 @@ package com.bhar32.practise.service;
 import com.bhar32.practise.model.Product;
 import com.bhar32.practise.repo.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -12,5 +17,14 @@ public class ProductService {
     private ProductRepository productRepository;
     public void addProduct(Product product) {
         productRepository.save(product);
+    }
+
+    public ResponseEntity<List<Product>> getAllProdcts() {
+        List<Product> productList = new ArrayList<>();
+        productRepository.findAll().forEach(productList::add);
+        if(productList.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(productList, HttpStatus.OK);
     }
 }
