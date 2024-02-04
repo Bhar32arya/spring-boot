e3dc3298de035762ef1b57bd9cbf171bab2c2cc3
Bhar32arya <bhar32arya@gmail.com>
2024-02-04 13:51:31 +0530

    added first test case

--- junit-mockito/src/main/java/com/bhar32/practise/service/TutorialService.java
+++ junit-mockito/src/main/java/com/bhar32/practise/service/TutorialService.java
@@ -3,28 +3,28 @@ package com.bhar32.practise.service;
 import com.bhar32.practise.model.Tutorial;
 import com.bhar32.practise.repository.TutorialRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.Optional;
 
 @Service
 public class TutorialService {
 
-    @Autowired(required = false)
-    private TutorialRepository productRepository;
-    public ResponseEntity<Tutorial> addProduct(Tutorial product) {
-        productRepository.save(product);
-        return new ResponseEntity<>(product, HttpStatus.CREATED);
+    @Autowired
+    private TutorialRepository tutorialRepository;
+
+    public String buildId(long id, String title) {
+        if(title.startsWith("J")) {
+            return "Java" + id + title;
+        }
+        return id + title;
     }
 
-    public ResponseEntity<List<Tutorial>> getAllProducts() {
-        List<Tutorial> productList = new ArrayList<>();
-        productRepository.findAll().forEach(productList::add);
-        if(productList.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    public boolean checkTutorialAlreadyExist(Long id) {
+        Optional<Tutorial> optionalTutorial = tutorialRepository.findById(id.longValue());
+        if(optionalTutorial.isPresent()) {
+            return true;
         }
-        return new ResponseEntity<>(productList, HttpStatus.OK);
+        return false;
     }
 }
--- junit-mockito/src/test/java/com/bhar32/practise/JunitMockitoApplicationTests.java
+++ junit-mockito/src/test/java/com/bhar32/practise/JunitMockitoApplicationTests.java
@@ -1,7 +1,9 @@
 package com.bhar32.practise;
 
+import com.bhar32.practise.service.TutorialService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class JunitMockitoApplicationTests {
@@ -10,6 +12,13 @@ class JunitMockitoApplicationTests {
 	void contextLoads() {
 	}
 
-
+	@Test
+	public void checkBuildIdLogic() {
+		TutorialService tutorialService = new TutorialService();
+		String builtId = tutorialService.buildId(123, "Spring");
+		String builtIdTwo = tutorialService.buildId(456, "Java");
+		assertEquals("123Spring", builtId);
+		assertEquals("Java456Java", builtIdTwo);
+	}
 
 }
